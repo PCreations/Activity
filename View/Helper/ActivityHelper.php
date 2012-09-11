a260d73aeae066f92d9996124c929f4ff0a5bde0
Pierre Criulanscy <pcriulan@gmail.com>
2012-09-11 03:55:38 +0200

    Add ActivityHelper::init() method to performs channel subscriptions to pusher according to database channel subscriptions

--- View/Helper/ActivityHelper.php
+++ View/Helper/ActivityHelper.php
@@ -4,8 +4,13 @@ class ActivityHelper extends AppHelper {
 	
 	public $helpers = array('Pusher.Pusher', 'Html', 'Js');
 
-	public function init() {
-		
+	public function init($subscribedChannels) {
+		foreach($subscribedChannels as $channel) {
+			$this->Pusher->subscribe($channel['ActivityChannel']['name'], 'private');
+			foreach($channel['ActivityEvent'] as $event) {
+				$this->Pusher->bindEvent($channel['ActivityChannel']['name'], $event['name'], "console.log(data.message);")
+			}
+		}
 	}
 
 }
